3fd36d6947c7e9f2fa20122a378fc709f2a9b3b7
AleksandrK <4d.41.49.4c@gmail.com>
2022-11-09 13:40:16 +0500

    feat: find sum in the middle ot the list

--- TaskSearch/Searcher.cs
+++ TaskSearch/Searcher.cs
@@ -10,9 +10,43 @@ namespace TaskSearch
     {
         public void FindElementsForSum(List<uint> list, ulong sum, out int start, out int end)
         {
+            if (list is null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            int max = list.Count;   //maximim allowed end variable value
             start = 0;
             end = 0;
-            throw new NotImplementedException();
+            if (max == 0) return;   //list is emply, nothing to search for
+
+            ulong temp_sum = 0;     //sum of elements between start and end
+
+            for (; end < max;)
+            {
+                if (temp_sum == sum)
+                {
+                    //match found
+                    return;
+                }
+                else if (temp_sum < sum)
+                {
+                    //grow right = add next element & mind the right boundary
+                    temp_sum += list[end];
+                    end++;
+                    continue;
+                }
+                else if (temp_sum > sum)
+                {
+                    //trim left = remove first element
+                    temp_sum -= list[start];
+                    start++;
+                    continue;
+                }
+            }
+
+            start = 0;
+            end = 0;
         }
     }
 }
--- TaskSearchTest/SearcherTest.cs
+++ TaskSearchTest/SearcherTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using TaskSearch;
 
 namespace TaskSearchTest
@@ -8,12 +9,48 @@ namespace TaskSearchTest
     public class SearcherTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SearchInTheMiddle()
         {
+            var list = new List<uint> { 0, 1, 2, 3, 4, 5, 6, 7 };
+            ulong sum = 11;
+            //start будет равен 5 и end 7
+
+            int start = 0, end = 0;
             Searcher searcher = new Searcher();
-            int start = 7, end = 7;
-            searcher.FindElementsForSum(null, 0, out start, out end);
-            Assert.AreEqual(3, start, "Not three!");
+            searcher.FindElementsForSum(list, sum, out start, out end);
+
+            Assert.AreEqual(5, start, "Start must be 5");
+            Assert.AreEqual(7, end, "End must be 7");
+        }
+
+        [TestMethod]
+        public void SearchFromMiddleToEnd()
+        {
+            var list = new List<uint> { 4, 5, 6, 7 };
+            ulong sum = 18;
+            //start будет равен 1 и end 4
+
+            int start = 0, end = 0;
+            Searcher searcher = new Searcher();
+            searcher.FindElementsForSum(list, sum, out start, out end);
+            //TODO both 0's, sum wasn't found
+            Assert.AreEqual(1, start, "Start must be 1");
+            Assert.AreEqual(4, end, "End must be 4");
+        }
+
+        [TestMethod]
+        public void SearchTooBigSum()
+        {
+            var list = new List<uint> { 0, 1, 2, 3, 4, 5, 6, 7 };
+            ulong sum = 88;
+            //start будет равен 0 и end 0
+
+            int start = 0, end = 0;
+            Searcher searcher = new Searcher();
+            searcher.FindElementsForSum(list, sum, out start, out end);
+
+            Assert.AreEqual(0, start, "Start must be 0");
+            Assert.AreEqual(0, end, "End must be 0");
         }
     }
 }
