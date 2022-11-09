890d7cc705968ee04e6c4adcb289fd67b6f197d1
AleksandrK <4d.41.49.4c@gmail.com>
2022-11-09 12:07:52 +0500

    Create project (DLL+MSTest)

---
+++ TaskSearch/Searcher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSearch
+{
+    public class Searcher
+    {
+        public void FindElementsForSum(List<uint> list, ulong sum, out int start, out int end)
+        {
+            start = 0;
+            end = 0;
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TaskSearchTest/SearcherTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TaskSearch;
+
+namespace TaskSearchTest
+{
+    [TestClass]
+    public class SearcherTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Searcher searcher = new Searcher();
+            int start = 7, end = 7;
+            searcher.FindElementsForSum(null, 0, out start, out end);
+            Assert.AreEqual(3, start, "Not three!");
+        }
+    }
+}
