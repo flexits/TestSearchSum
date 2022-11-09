effccef3a9da9aed74335903904a052456b57d3c
AleksandrK <4d.41.49.4c@gmail.com>
2022-11-09 15:45:43 +0500

    feat: Full implementation and tests

--- TaskSearch/Searcher.cs
+++ TaskSearch/Searcher.cs
@@ -14,37 +14,60 @@ namespace TaskSearch
             {
                 throw new ArgumentNullException();
             }
-
-            int max = list.Count;   //maximim allowed end variable value
+            
+            //init indices
             start = 0;
             end = 0;
+            
+            //find the right boundary
+            int max = list.Count;
             if (max == 0) return;   //list is emply, nothing to search for
+            
+            //temporary sum of a fragment of the list
+            ulong temp_sum = 0;
 
-            ulong temp_sum = 0;     //sum of elements between start and end
-
-            for (; end < max;)
+            for (; ;)
             {
                 if (temp_sum == sum)
                 {
-                    //match found
-                    return;
+                    if (start == end)
+                    {
+                        //starting calculation for a fragment
+                        temp_sum = list[start];
+                        end++;
+                        if (end == max) break; //TODO make the end condition check not so ugly
+                    }
+                    else
+                    {
+                        //the match found
+                        return;
+                    }
                 }
                 else if (temp_sum < sum)
                 {
                     //grow right = add next element & mind the right boundary
-                    temp_sum += list[end];
-                    end++;
-                    continue;
+                    if (end < max)
+                    {
+                        temp_sum += list[end++];
+                    }
+                    else
+                    {
+                        //the right boundary is reached and the sum is still not found - exit
+                        break; //TODO make the end condition check not so ugly
+                    }
                 }
                 else if (temp_sum > sum)
                 {
-                    //trim left = remove first element
-                    temp_sum -= list[start];
-                    start++;
-                    continue;
+                    //trim left = remove first element & mind the end position
+                    if (start < end)
+                    {
+                        temp_sum -= list[start++];
+                    }
+                    //eventually start will become equal to end, sum will be 0 at the time,
+                    //then we'll move rightward to a next fragment of the list
                 }
             }
-
+            //there was no match found, reset indices
             start = 0;
             end = 0;
         }
--- TaskSearchTest/SearcherTest.cs
+++ TaskSearchTest/SearcherTest.cs
@@ -9,15 +9,136 @@ namespace TaskSearchTest
     public class SearcherTest
     {
         [TestMethod]
+        public void CheckIfListNull()
+        {
+            //must throw ArgumentNullException
+            _ = Assert.ThrowsException<ArgumentNullException>(() =>
+                {
+                    new Searcher().FindElementsForSum(null, 0, out _, out _);
+                });
+        }
+
+        [TestMethod]
+        public void CheckIfListEmpty()
+        {
+            var list = new List<uint>();
+            ulong sum = 88;
+            //start будет равен 0 и end 0
+
+            new Searcher().FindElementsForSum(
+                list, sum, out int start, out int end);
+
+            Assert.AreEqual(0, start, "Start must be 0");
+            Assert.AreEqual(0, end, "End must be 0");
+        }
+
+        [TestMethod]
+        public void SearchTooBigSum()
+        {
+            var list = new List<uint> { 0, 1, 2, 3, 4, 5, 6, 7 };
+            ulong sum = 88;
+            //start будет равен 0 и end 0
+
+            new Searcher().FindElementsForSum(
+                list, sum, out int start, out int end);
+
+            Assert.AreEqual(0, start, "Start must be 0");
+            Assert.AreEqual(0, end, "End must be 0");
+        }
+
+        [TestMethod]
+        public void SearchTooSmallSum()
+        {
+            var list = new List<uint> { 10, 11, 12, 13, 14, 15, 16, 17 };
+            ulong sum = 3;
+            //start будет равен 0 и end 0
+
+            new Searcher().FindElementsForSum(
+                list, sum, out int start, out int end);
+
+            Assert.AreEqual(0, start, "Start must be 0");
+            Assert.AreEqual(0, end, "End must be 0");
+        }
+
+        [TestMethod]
+        public void SearchSingleElement()
+        {
+            var list = new List<uint> { 255 };
+            ulong sum = 255;
+            //start будет равен 0 и end 1
+
+            new Searcher().FindElementsForSum(
+                list, sum, out int start, out int end);
+
+            Assert.AreEqual(0, start, "Start must be 0");
+            Assert.AreEqual(1, end, "End must be 1");
+        }
+
+        [TestMethod]
+        public void SearchNoSingleElement()
+        {
+            var list = new List<uint> { 255 };
+            ulong sum = 256;
+            //start будет равен 0 и end 0
+
+            new Searcher().FindElementsForSum(
+                list, sum, out int start, out int end);
+
+            Assert.AreEqual(0, start, "Start must be 0");
+            Assert.AreEqual(0, end, "End must be 0");
+        }
+
+        [TestMethod]
+        public void SearchZeroInZeroes()
+        {
+            var list = new List<uint> { 0, 0, 0, 0, 0};
+            ulong sum = 0;
+            //start будет равен 0 и end 1
+
+            new Searcher().FindElementsForSum(
+                list, sum, out int start, out int end);
+
+            Assert.AreEqual(0, start, "Start must be 0");
+            Assert.AreEqual(1, end, "End must be 1");
+        }
+
+        [TestMethod]
+        public void SearchZeroInNonZeroes()
+        {
+            var list = new List<uint> { 4, 5, 6, 7, 8, 9 };
+            ulong sum = 0;
+            //start будет равен 0 и end 0
+
+            new Searcher().FindElementsForSum(
+                list, sum, out int start, out int end);
+
+            Assert.AreEqual(0, start, "Start must be 0");
+            Assert.AreEqual(0, end, "End must be 0");
+        }
+
+        [TestMethod]
+        public void SearchNonZeroInZeroes()
+        {
+            var list = new List<uint> { 0, 0, 0, 0, 0 };
+            ulong sum = 7;
+            //start будет равен 0 и end 0
+
+            new Searcher().FindElementsForSum(
+                list, sum, out int start, out int end);
+
+            Assert.AreEqual(0, start, "Start must be 0");
+            Assert.AreEqual(0, end, "End must be 0");
+        }
+
+        [TestMethod]
         public void SearchInTheMiddle()
         {
             var list = new List<uint> { 0, 1, 2, 3, 4, 5, 6, 7 };
             ulong sum = 11;
             //start будет равен 5 и end 7
 
-            int start = 0, end = 0;
-            Searcher searcher = new Searcher();
-            searcher.FindElementsForSum(list, sum, out start, out end);
+            new Searcher().FindElementsForSum(
+                list, sum, out int start, out int end);
 
             Assert.AreEqual(5, start, "Start must be 5");
             Assert.AreEqual(7, end, "End must be 7");
@@ -30,24 +151,86 @@ namespace TaskSearchTest
             ulong sum = 18;
             //start будет равен 1 и end 4
 
-            int start = 0, end = 0;
-            Searcher searcher = new Searcher();
-            searcher.FindElementsForSum(list, sum, out start, out end);
-            //TODO both 0's, sum wasn't found
+            new Searcher().FindElementsForSum(
+                list, sum, out int start, out int end);
+            
             Assert.AreEqual(1, start, "Start must be 1");
             Assert.AreEqual(4, end, "End must be 4");
         }
 
         [TestMethod]
-        public void SearchTooBigSum()
+        public void SearchInTwoParts()
         {
-            var list = new List<uint> { 0, 1, 2, 3, 4, 5, 6, 7 };
-            ulong sum = 88;
-            //start будет равен 0 и end 0
+            var list = new List<uint> { 0, 1, 2, 333, 4, 5, 6, 7, 8 };
+            ulong sum = 11;
+            //start будет равен 5 и end 7
 
-            int start = 0, end = 0;
-            Searcher searcher = new Searcher();
-            searcher.FindElementsForSum(list, sum, out start, out end);
+            new Searcher().FindElementsForSum(
+                list, sum, out int start, out int end);
+
+            Assert.AreEqual(5, start, "Start must be 5");
+            Assert.AreEqual(7, end, "End must be 7");
+        }
+
+        [TestMethod]
+        public void Search1MList()
+        {
+            int length = 1000000;
+            var list = new List<uint>(length);
+            var rand = new Random();
+            for (int count = length; count > 0; count--)
+            {
+                list.Add((uint)rand.Next()+1);
+            }
+            //takes 18ms
+            ulong sum = 0;
+            //as all items are > 0, the sum will not be found
+
+            new Searcher().FindElementsForSum(
+                list, sum, out int start, out int end);
+            //takes 46-18 = 28ms
+
+            Assert.AreEqual(0, start, "Start must be 0");
+            Assert.AreEqual(0, end, "End must be 0");
+        }
+
+        [TestMethod]
+        public void Search3MList()
+        {
+            int length = 3000000;
+            var list = new List<uint>(length);
+            var rand = new Random();
+            for (int count = length; count > 0; count--)
+            {
+                list.Add((uint)rand.Next() + 1);
+            }
+            //takes 43ms
+            ulong sum = 0;
+
+            new Searcher().FindElementsForSum(
+                list, sum, out int start, out int end);
+            //takes 84-43 = 41ms
+
+            Assert.AreEqual(0, start, "Start must be 0");
+            Assert.AreEqual(0, end, "End must be 0");
+        }
+
+        [TestMethod]
+        public void Search10MList()
+        {
+            int length = 10000000;
+            var list = new List<uint>(length);
+            var rand = new Random();
+            for (int count = length; count > 0; count--)
+            {
+                list.Add((uint)rand.Next() + 1);
+            }
+            //takes 137ms
+            ulong sum = 0;
+
+            new Searcher().FindElementsForSum(
+                list, sum, out int start, out int end);
+            //takes 283-137 = 146ms
 
             Assert.AreEqual(0, start, "Start must be 0");
             Assert.AreEqual(0, end, "End must be 0");
